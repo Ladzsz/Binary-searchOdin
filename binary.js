c7edf4f26f5ae07731d14781a1e82eb60d98ae74
Ladzsz <londonmason602@gail.com>
2024-12-11 10:39:18 -0500

    created insert and remove methods

--- binary.js
+++ binary.js
@@ -61,17 +61,92 @@ class Tree {
     }
 
     //insert method
-    insertItem(value) {
+    insertItem(root, key) {
+        // Create and return a new node if root is null
+        if (root === null)
+        return new Node(key); 
 
+        // Duplicates not allowed
+        if (root.data === key)
+            return root; 
+
+        // Add to left subtree if less than and right if greater
+        if (key < root.data)
+            root.left = this.insertItem(root.left, key); 
+        else if (key > root.data)
+            root.right = this.insertItem(root.right, key); 
+
+        return root;
     }
 
     //remove method
-    removeItem(value) {
+    removeItem(root, x) {
+        // Base case
+        if (root === null) {
+            return root;
+        }
 
+        // If the key to be removed is smaller than the root's key
+        if (x < root.data) {
+            root.left = this.removeItem(root.left, x);
+        }
+        // If the key to be removed is greater than the root's key
+        else if (x > root.data) {
+            root.right = this.removeItem(root.right, x);
+        }
+        // If the key matches the root's key
+        else {
+            // Node with only one child or no child
+            if (root.left === null) 
+                return root.right;
+
+            if (root.right === null) 
+                return root.left;
+
+            // Node with two children: get the in-order successor
+            let succ = this.getSuccessor(root);
+
+            // Copy the successor's value to this node
+            root.data = succ.data;
+
+            // Delete the successor
+            root.right = this.removeItem(root.right, succ.data);
+        }
+        return root;
+    }
+
+    // Helper method to find the in-order successor (smallest node in the right subtree)
+    getSuccessor(curr) {
+        curr = curr.right;
+        while (curr !== null && curr.left !== null) {
+            curr = curr.left;
+        }
+        return curr;
     }
 
     //find value method
-    find(value) {
+    find(root, key) {
+
+        // Base case: root is null or key is found
+        if (root === null || root.data === key) {
+            return root;
+        }
+
+        // Key is smaller than root's data: search in the left subtree
+        if (key < root.data) {
+            return this.find(root.left, key);
+        }
+
+        // Key is greater than root's data: search in the right subtree
+        return this.find(root.right, key);
+    }
+
+    //functions to work on next time
+    levelOrder() {
+
+    }
+
+    inOrder() {
 
     }
 }
